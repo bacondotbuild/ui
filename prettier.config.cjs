232280a41a07fb3bde57121b561a6a0291d76872
bacon.build <bacondotbuild@gmail.com>
2023-02-28 19:34:28 -0800

    add prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Config} */
+const config = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  trailingComma: "es5",
+  arrowParens: "avoid",
+  endOfLine: "auto",
+  semi: false,
+};
+
+module.exports = config;
