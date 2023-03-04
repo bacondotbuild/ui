ba65312bb569cfea46001feae66da90d219a9f45
bacon.build <bacondotbuild@gmail.com>
2023-03-03 22:05:06 -0800

    move into src directory
    - update scripts
    - update readme

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 ## using [The 30-second guide to publishing a TypeScript package to NPM](https://cameronnokes.com/blog/the-30-second-guide-to-publishing-a-typescript-package-to-npm/)
 
-
-
 ## matt's vid
 
 ~~based on [this video from matt pocock](https://www.youtube.com/watch?v=eh89VE3Mk5g)~~
 
 initial commit [failed](https://github.com/bacondotbuild/ui/actions/runs/4299469346)
+
+## todo
+
+- TODO: add toast/notification
+- TODO: side nav menu
+- TODO: command palette
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import Button from '@/components/design/button'
+import Footer from '@/components/design/footer'
+import Header from '@/components/design/header'
+import Main from '@/components/design/main'
+import Page from '@/components/design/page'
+import Title from '@/components/design/title'
+
+import DragDropList from '@/components/dragDropList'
+import LoadingIcon from '@/components/loading-icon'
+import Loading from '@/components/loading'
+import Modal from '@/components/modal'
+
+export {
+  Button,
+  Footer,
+  Header,
+  Main,
+  Page,
+  Title,
+  DragDropList,
+  LoadingIcon,
+  Loading,
+  Modal,
+}
